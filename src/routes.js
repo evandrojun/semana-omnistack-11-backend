c112f32c3e64fcd25468b517cd35b479ac54f7aa
Evandro Ishikawa <evandroishikawa@usp.br>
2020-03-27 01:21:33 -0300

    Finalize backend application

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngController = require('./controllers/ong_controller');
+const IncidentController = require('./controllers/incident_controller');
+const ProfileController = require('./controllers/ongs/profile_controller');
+const SessionController = require('./controllers/ongs/session_controller');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('ong-profile', ProfileController.index);
+
+module.exports = routes;
